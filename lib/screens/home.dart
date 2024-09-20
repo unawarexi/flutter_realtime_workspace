78cb29a49de313ced73bcdf361310bec94a09a8c
unawarexi <unawarexi@gmail.com>
2024-09-20 14:57:38 +0100

    updated the home ui

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -11,7 +11,6 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey[100],
       body: SingleChildScrollView(
-        // Wrap the entire content inside SingleChildScrollView
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
@@ -21,132 +20,31 @@ class Home extends StatelessWidget {
 
               // Welcome Text
               const Text(
-                'Hello, User', // Adjust to use dynamic data if needed
+                'Hello, User',
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
               const SizedBox(height: 20),
 
-              // Search Bar with fully rounded edges
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(30),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: const Offset(0, 3), // Shadow position
-                    ),
-                  ],
-                ),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Search...',
-                    prefixIcon: const Icon(Icons.search),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide.none,
-                    ),
-                    filled: true,
-                    fillColor: Colors.white,
-                    contentPadding: const EdgeInsets.symmetric(vertical: 15),
-                  ),
-                ),
-              ),
+              // Search Bar
+              _buildSearchBar(),
 
               const SizedBox(height: 50),
 
               // Quick Access Section
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Quick Access',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey[700],
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      // Action for 'Add items' link
-                    },
-                    child: const Text(
-                      'Add items',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.blue,
-                        decoration: TextDecoration.underline,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+              _buildQuickAccessSection(),
+
               const SizedBox(height: 10),
 
-              // Personalized Space with Image from Assets
-              Row(
-                children: [
-                  // Placeholder Image (From Assets)
-                  Container(
-                    width: screenWidth * 0.4,
-                    height: 180,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.grey[300],
-                    ),
-                    child: Image.asset(
-                      'assets/images/personalized.png', // Replace with your asset image path
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  const SizedBox(width: 20),
+              // Personalized Space
+              _buildPersonalizedSpace(screenWidth),
 
-                  // Personalized Text
-                  Container(
-                    width:
-                        screenWidth * 0.4, // Adjust the width to fit properly
-                    child: const Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Personalize this space',
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black87,
-                          ),
-                        ),
-                        SizedBox(height: 5),
-                        Text(
-                          'Add your most important stuff here for fast access',
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
               const SizedBox(height: 50),
 
               // List Tiles for Sections
-              const ListTile(
-                leading: Icon(Icons.folder, color: Colors.blue),
-                title: Text('Collaboration'),
-                subtitle: Text('Modified: 12 Sept 2024'),
-                trailing: Icon(Icons.more_vert),
-              ),
-              const ListTile(
-                leading: Icon(Icons.folder, color: Colors.blue),
-                title: Text('My Open Issues'),
-                subtitle: Text('Modified: 10 Sept 2024'),
-                trailing: Icon(Icons.more_vert),
-              ),
+              _buildSectionListTile(
+                  Icons.folder, 'Collaboration', 'Modified: 12 Sept 2024'),
+              _buildSectionListTile(
+                  Icons.folder, 'My Open Issues', 'Modified: 10 Sept 2024'),
 
               const SizedBox(height: 50),
 
@@ -154,14 +52,14 @@ class Home extends StatelessWidget {
               Text(
                 'Collaborator Tools',
                 style: TextStyle(
-                  fontSize: 15,
+                  fontSize: 18,
                   fontWeight: FontWeight.bold,
                   color: Colors.grey[700],
                 ),
               ),
               const SizedBox(height: 30),
 
-              // Build Collaboration Options
+              // Collaboration Options
               _buildCollabOptions(context),
             ],
           ),
@@ -170,7 +68,137 @@ class Home extends StatelessWidget {
     );
   }
 
-  // Function to build Collab Section with clickable icons
+  // Function to build the Search Bar with neomorphism
+  Widget _buildSearchBar() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 15,
+            offset: const Offset(0, 5),
+          ),
+        ],
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: 'Search...',
+          prefixIcon: const Icon(Icons.search, color: Colors.blue),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+            borderSide: BorderSide.none,
+          ),
+          filled: true,
+          fillColor: Colors.white,
+          contentPadding: const EdgeInsets.symmetric(vertical: 15),
+        ),
+      ),
+    );
+  }
+
+  // Function to build Quick Access Section
+  Widget _buildQuickAccessSection() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'Quick Access',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.grey[700],
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            // Action for 'Add items' link
+          },
+          child: const Text(
+            'Add items',
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.blue,
+              decoration: TextDecoration.underline,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  // Function to build Personalized Space
+  Widget _buildPersonalizedSpace(double screenWidth) {
+    return Row(
+      children: [
+        // Placeholder Image (From Assets)
+        Container(
+          width: screenWidth * 0.4,
+          height: 180,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.grey[300],
+            // boxShadow: [
+            //   BoxShadow(
+            //     color: Colors.grey.withOpacity(0.5),
+            //     spreadRadius: 5,
+            //     blurRadius: 10,
+            //     offset: const Offset(0, 5),
+            //   ),
+            // ],
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.asset(
+              'assets/images/personalized.png', // Replace with your asset image path
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        const SizedBox(width: 20),
+
+        // Personalized Text
+        Container(
+          width: screenWidth * 0.4,
+          child: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Personalize this space',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                ),
+              ),
+              SizedBox(height: 5),
+              Text(
+                'Add your most important stuff here for fast access',
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  // Function to build section list tiles
+  Widget _buildSectionListTile(IconData icon, String title, String subtitle) {
+    return ListTile(
+      leading: Icon(icon, color: Colors.blue),
+      title: Text(title),
+      subtitle: Text(subtitle),
+      trailing: Icon(Icons.more_vert),
+    );
+  }
+
+  // Function to build Collaborator Tools Section
   Widget _buildCollabOptions(BuildContext context) {
     return Column(
       children: [
@@ -193,22 +221,54 @@ class Home extends StatelessWidget {
               MaterialPageRoute(builder: (context) => const GoogleMapScreen()),
             );
           },
-          child: const Text('Chill Spots Nearby'),
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            padding: const EdgeInsets.symmetric(
+                vertical: 15,
+                horizontal: 30), // Increased padding for a neater look
+            backgroundColor: Colors.blue,
+            elevation: 5, // Adds shadow for a modern feel
+          ),
+          child: const Text(
+            'Chill Spots Nearby',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16, // Increased font size for better readability
+              fontWeight: FontWeight.bold, // Bold text for emphasis
+            ),
+          ),
         ),
       ],
     );
   }
 
-  // Function to build individual collab options
+  // Function to build individual collab options with neomorphic effect
   Widget _buildCollabOption(
       BuildContext context, IconData icon, String label, String route) {
-    return ListTile(
-      leading: Icon(icon, color: Colors.blue),
-      title: Text(label),
-      trailing: const Icon(Icons.arrow_forward_ios),
-      onTap: () {
-        Navigator.pushNamed(context, route);
-      },
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 5),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15),
+        // boxShadow: [
+        //   BoxShadow(
+        //     color: Colors.grey.withOpacity(0.3),
+        //     spreadRadius: 5,
+        //     blurRadius: 15,
+        //     offset: const Offset(0, 5),
+        //   ),
+        // ],
+      ),
+      child: ListTile(
+        leading: Icon(icon, color: Colors.blue),
+        title: Text(label),
+        trailing: const Icon(Icons.arrow_forward_ios),
+        onTap: () {
+          Navigator.pushNamed(context, route);
+        },
+      ),
     );
   }
 }
