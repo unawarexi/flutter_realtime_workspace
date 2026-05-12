f14f1c5f99c45f6e27d5bc5b95c8be9f7d1a048e
unawarexi <unawarexi@gmail.com>
2026-05-12 17:30:38 +0100

    chore(deps): update pubspec, regenerate plugin registrants, update iOS config
    - Updated pubspec.yaml dependencies and regenerated pubspec.lock
    - Regenerated plugin registrants for linux, macos, windows
    - Updated iOS AppFrameworkInfo.plist, project.pbxproj, AppDelegate, Info.plist
    - Updated .gitignore

--- Flutter_Realtime_Workspace/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Flutter_Realtime_Workspace/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -22,6 +22,7 @@ import local_auth_darwin
 import msal_auth
 import package_info_plus
 import path_provider_foundation
+import sentry_flutter
 import shared_preferences_foundation
 import sqflite_darwin
 import url_launcher_macos
@@ -46,6 +47,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   MsalAuthPlugin.register(with: registry.registrar(forPlugin: "MsalAuthPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
--- Flutter_Realtime_Workspace/windows/flutter/generated_plugin_registrant.cc
+++ Flutter_Realtime_Workspace/windows/flutter/generated_plugin_registrant.cc
@@ -16,6 +16,7 @@
 #include <geolocator_windows/geolocator_windows.h>
 #include <local_auth_windows/local_auth_plugin.h>
 #include <permission_handler_windows/permission_handler_windows_plugin.h>
+#include <sentry_flutter/sentry_flutter_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
@@ -39,6 +40,8 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("LocalAuthPlugin"));
   PermissionHandlerWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
+  SentryFlutterPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("SentryFlutterPlugin"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
